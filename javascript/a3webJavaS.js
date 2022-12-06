21c241d300c0b12ee275a4efb05cf8ba37002278
AKoornneefFleming <adrian.koornneef@flemingcollege.ca>
2022-12-06 12:46:48 -0500

    AKButtonCommitt2

--- javascript/a3webJavaS.js
+++ javascript/a3webJavaS.js
@@ -4,7 +4,7 @@
 // * From  : https://codepen.io/Tcip/pen/BNKjeN
 //           https://flexiple.com/javascript/javascript-clock/
 //           https://developer.mozilla.org/en-US/docs/Web/CSS/border-radius
-//           https://www.w3schools.com/jsref/event_onclick.asp
+//            https://www.w3schools.com/jsref/event_onclick.asp
 /////////////////////////////////////////////////////////////////
   
 function myFunction() {
